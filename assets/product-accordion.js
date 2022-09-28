3ea074af3f8502f31582904f9bed2bb0d9d0ac6b
expldec <explosive.deconstruction@gmail.com>
2022-09-29 00:50:09 +0200

    added an accordion to product details. Working on making it a reusable component

--- assets/product-accordion.js
+++ assets/product-accordion.js
@@ -2,7 +2,9 @@ if (!customElements.get('product-accordion')) {
   customElements.define('product-accordion', class ProductAccordion extends HTMLElement {
     constructor() {
       super();
-      
+      // this.querySelectorAll('div').forEach(
+      //   (div) => div.addEventListener('click', this.onCaretClick.bind(this))
+      // );
     }
 
     onCaretClick(event) {
